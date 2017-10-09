1b89bb9ab8297db1df401c78f59e59b718707ab7
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2017-10-09 09:41:08 +0200

    django 2.0 compatibility, increased version to 0.9.2

--- django_rest_passwordreset/urls.py
+++ django_rest_passwordreset/urls.py
@@ -3,6 +3,8 @@
 from django.conf.urls import url, include
 from django_rest_passwordreset.views import reset_password_request_token, reset_password_confirm
 
+app_name = 'password_reset'
+
 urlpatterns = [
     url(r'^confirm', reset_password_confirm, name="reset-password-confirm"),
     url(r'^', reset_password_request_token, name="reset-password-request"),
