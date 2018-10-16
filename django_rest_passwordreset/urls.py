249eec01a7fba7be40bf41119142039ddbe874e1
anx-abruckner <ABruckner@anexia-it.com>
2018-10-16 15:46:15 +0200

    Update urls.py
    add trailing slash to reset-password-confirm url

--- django_rest_passwordreset/urls.py
+++ django_rest_passwordreset/urls.py
@@ -6,6 +6,6 @@ from django_rest_passwordreset.views import reset_password_request_token, reset_
 app_name = 'password_reset'
 
 urlpatterns = [
-    url(r'^confirm', reset_password_confirm, name="reset-password-confirm"),
+    url(r'^confirm/', reset_password_confirm, name="reset-password-confirm"),
     url(r'^', reset_password_request_token, name="reset-password-request"),
 ]
