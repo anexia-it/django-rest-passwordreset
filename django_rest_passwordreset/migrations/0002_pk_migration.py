anexia-it/django-rest-passwordreset

commit 68306e428f196499227c16ed6409b1f94782490e

Potential fix for issue #8 (Django 2.1 and multiple PKs)

django_rest_passwordreset/migrations/0002_pk_migration.py
@@ -22,26 +22,21 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='resetpasswordtoken',
-            name='id',
-            field=models.IntegerField(null=True),
-            preserve_default=True,
-        ),
-        migrations.RunPython(
-            populate_auto_incrementing_pk_field,
-            migrations.RunPython.noop
-        ),
-        # add primary key information to id field
-        migrations.AlterField(
-            model_name='resetpasswordtoken',
-            name='id',
-            field=models.AutoField(primary_key=True, serialize=False)
-        ),
         # remove primary key information from 'key' field
         migrations.AlterField(
             model_name='resetpasswordtoken',
             name='key',
-            field=models.CharField(db_index=True, max_length=64, unique=True, verbose_name='Key'),
+            field=models.CharField(db_index=True, primary_key=False, max_length=64, unique=True, verbose_name='Key'),
+        ),
+        # add a new id field
+        migrations.AddField(
+            model_name='resetpasswordtoken',
+            name='id',
+            field=models.AutoField(primary_key=True, serialize=False),
+            preserve_default=False,
+        ),
+        migrations.RunPython(
+            populate_auto_incrementing_pk_field,
+            migrations.RunPython.noop
         ),
     ]

setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-passwordreset',
-    version='0.9.5',
+    version='0.9.6',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
@@ -27,6 +27,7 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',

.travis.yml
@@ -11,14 +11,20 @@ env:
   - DJANGO=1.8
   - DJANGO=1.11
   - DJANGO=2.0
+  - DJANGO=2.1
   - DJANGO=master
 matrix:
   exclude:
     # Django >= 2.0 does not support Python2.7/PyPy2 anymore
     - { python: "2.7", env: DJANGO=2.0 }
     - { python: "pypy", env: DJANGO=2.0 }
+    - { python: "2.7", env: DJANGO=2.1 }
+    - { python: "pypy", env: DJANGO=2.1 }
     - { python: "2.7", env: DJANGO=master }
     - { python: "pypy", env: DJANGO=master }
+    # Django >= 2.1 does not support Python3.4 anymore
+    - { python: "3.4", env: DJANGO=2.1 }
+    - { python: "3.4", env: DJANGO=master }
   allow_failures:
     # newer python versions aswell as pypy may also always break our build
     - python: "pypy"

tests/urls.py
@@ -1,6 +1,8 @@
 """ Tests App URL Config """
 from django.conf.urls import url, include
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    url(r'^admin/', admin.site.urls),
 ]

tox.ini
@@ -3,6 +3,7 @@ envlist =
        {py27,py34,py35,pypy,pypy3}-django18-{drf35,drf36},
        {py27,py34,py35,py36,pypy,pypy3}-django111-{drf35,drf36,drf37,drf38},
        {py34,py35,py36,pypy3}-django20-{drf37,drf38},
+       {py35,py36,pypy3}-django21-{drf37,drf38},
        {py35,py36,pypy3}-djangomaster-{drf38}
 
 [travis:env]
@@ -10,6 +11,7 @@ DJANGO =
     1.8: django18
     1.11: django111
     2.0: django20
+    2.1: django21
     master: djangomaster
 
 [testenv]
@@ -28,4 +30,5 @@ deps =
         django18: Django>=1.8,<1.9
         django111: Django>=1.11,<2.0
         django20: Django>=2.0,<2.1
+        django21: Django>=2.1,<2.2
         djangomaster: https://github.com/django/django/archive/master.tar.gz

commit 34b66f92a2dea6b484beb8a24cd9ffefd6cb6d7e

Fixed regression from 68306e428f196499227c16ed6409b1f94782490e / Issue #8 migrations broke again, but now for django 1.11 and 2.0

django_rest_passwordreset/migrations/0002_pk_migration.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import django
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
@@ -15,13 +16,37 @@ def populate_auto_incrementing_pk_field(apps, schema_editor):
         o.save()
 
 
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('django_rest_passwordreset', '0001_initial',),
+def get_migrations_for_django_before_21():
+    return [
+        # add a new id field (without primary key information)
+        migrations.AddField(
+            model_name='resetpasswordtoken',
+            name='id',
+            field=models.IntegerField(null=True),
+            preserve_default=True,
+        ),
+        # fill the new pk field
+        migrations.RunPython(
+            populate_auto_incrementing_pk_field,
+            migrations.RunPython.noop
+        ),
+        # add primary key information to id field
+        migrations.AlterField(
+            model_name='resetpasswordtoken',
+            name='id',
+            field=models.AutoField(primary_key=True, serialize=False)
+        ),
+        # remove primary key information from 'key' field
+        migrations.AlterField(
+            model_name='resetpasswordtoken',
+            name='key',
+            field=models.CharField(db_index=True, max_length=64, unique=True, verbose_name='Key'),
+        ),
     ]
 
-    operations = [
+
+def get_migrations_for_django_21_and_newer():
+    return [
         # remove primary key information from 'key' field
         migrations.AlterField(
             model_name='resetpasswordtoken',
@@ -40,3 +65,28 @@ class Migration(migrations.Migration):
             migrations.RunPython.noop
         ),
     ]
+
+
+def get_migrations_based_on_django_version():
+    """
+    Returns the proper migrations based on the current Django Version
+
+    Unfortunatley, Django 2.1 introduced a breaking change with switching PK from one model to another, see
+    https://code.djangoproject.com/ticket/29790
+    :return:
+    """
+    django_version = django.VERSION
+
+    if django_version[0] >= 2 and django_version[1] >= 1:
+        return get_migrations_for_django_21_and_newer()
+
+    return get_migrations_for_django_before_21()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('django_rest_passwordreset', '0001_initial',),
+    ]
+
+    operations = get_migrations_based_on_django_version()

setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-passwordreset',
-    version='0.9.6',
+    version='0.9.7',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',