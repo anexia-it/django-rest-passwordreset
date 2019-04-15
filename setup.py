7118d430d4b21f78a23530c88a33390c9b6a4f95
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2019-04-15 09:12:53 +0200

    Releasing version 1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-passwordreset',
-    version='1.0.0alpha3',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
