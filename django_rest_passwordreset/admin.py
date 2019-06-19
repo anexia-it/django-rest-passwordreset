1f38b12374856eaf63e4ef833e6f6e410159951c
Manuel Juarez <manuel@kondosoft.com>
2019-06-19 10:49:29 -0500

    dont show the ip address in the django admin

--- django_rest_passwordreset/admin.py
+++ django_rest_passwordreset/admin.py
@@ -5,4 +5,4 @@ from django_rest_passwordreset.models import ResetPasswordToken
 
 @admin.register(ResetPasswordToken)
 class ResetPasswordTokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'key', 'created_at', 'ip_address', 'user_agent')
+    list_display = ('user', 'key', 'created_at', 'user_agent')
