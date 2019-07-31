4f58ecd0a4fa35212a9c73d0584adcdd50953317
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2019-07-31 15:45:15 +0200

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-passwordreset',
-    version='1.1.0rc1',
+    version='1.1.0rc2',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
