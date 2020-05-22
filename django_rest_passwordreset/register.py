7abd202d9c11b66bc6ae4439d1e3dea16da8716b
Cole Calhoun <calhouncole@gmail.com>
2020-05-21 23:31:51 -0700

    cleanup of register.py imports

--- django_rest_passwordreset/register.py
+++ django_rest_passwordreset/register.py
@@ -1,21 +1,9 @@
-from datetime import timedelta
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.contrib.auth.password_validation import validate_password, get_password_validators
-from django.utils.translation import ugettext_lazy as _
-from django.utils import timezone
-from django.conf import settings
-from rest_framework import status, serializers, exceptions
-from rest_framework.generics import GenericAPIView
-from rest_framework.response import Response
+from django_rest_passwordreset.models import ResetPasswordToken, clear_user_tokens
+from django_rest_passwordreset.signals import reset_password_token_created
 
-from django_rest_passwordreset.serializers import EmailSerializer, PasswordTokenSerializer, TokenSerializer
-from django_rest_passwordreset.models import ResetPasswordToken, clear_expired, get_password_reset_token_expiry_time, \
-    get_password_reset_lookup_field, clear_user_tokens
-from django_rest_passwordreset.signals import reset_password_token_created, pre_password_reset, post_password_reset
 
 User = get_user_model()
-
 
 def create_registration_token(self, user):
     """
@@ -26,7 +14,7 @@ def create_registration_token(self, user):
 
     # find a user by pk
     user = User.objects.get(pk=user)
-    
+
     if user.eligible_for_reset(register_token=True):
         #clear the user's tokens
         clear_user_tokens(user)
