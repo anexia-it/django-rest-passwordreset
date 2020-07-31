e922f6528a1925b3020d45c07913432a11995356
bomba1990 <marianoramirez353@gmail.com>
2020-07-31 10:39:33 -0300

    Moved some validations to the serializer

--- django_rest_passwordreset/serializers.py
+++ django_rest_passwordreset/serializers.py
@@ -1,6 +1,14 @@
-from django.utils.translation import gettext_lazy as _
+from datetime import timedelta
 
+from django.core.exceptions import ValidationError
+from django.http import Http404
+from django.shortcuts import get_object_or_404 as _get_object_or_404
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
+
+from django_rest_passwordreset.models import get_password_reset_token_expiry_time
+from . import models
 
 __all__ = [
     'EmailSerializer',
@@ -13,10 +21,35 @@ class EmailSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
 
-class PasswordTokenSerializer(serializers.Serializer):
+class PasswordValidateMixin:
+    def validate(self, data):
+        token = data.get('token')
+
+        # get token validation time
+        password_reset_token_validation_time = get_password_reset_token_expiry_time()
+
+        # find token
+        try:
+            reset_password_token = _get_object_or_404(models.ResetPasswordToken, key=token)
+        except (TypeError, ValueError, ValidationError, Http404,
+                models.ResetPasswordToken.DoesNotExist):
+            raise Http404(_("The OTP password entered is not valid. Please check and try again."))
+
+        # check expiry date
+        expiry_date = reset_password_token.created_at + timedelta(
+            hours=password_reset_token_validation_time)
+
+        if timezone.now() > expiry_date:
+            # delete expired token
+            reset_password_token.delete()
+            raise Http404(_("The token has expired"))
+        return data
+
+
+class PasswordTokenSerializer(PasswordValidateMixin, serializers.Serializer):
     password = serializers.CharField(label=_("Password"), style={'input_type': 'password'})
     token = serializers.CharField()
 
 
-class ResetTokenSerializer(serializers.Serializer):
+class ResetTokenSerializer(PasswordValidateMixin, serializers.Serializer):
     token = serializers.CharField()
