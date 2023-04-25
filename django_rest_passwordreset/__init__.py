b8b3839dd2f75a20303e5129c0755d8cee38cb6f
Ezeudoh Tochukwu <ezeudoh.tochukwu@gmail.com>
2023-04-25 17:35:33 +0100

    dropped support for py36

--- django_rest_passwordreset/__init__.py
+++ django_rest_passwordreset/__init__.py
@@ -1,4 +1,4 @@
 """ An extension of django ninja framework, providing a configurable password reset strategy"""
-__version__ = "1.0.0"
+__version__ = "1.0.2"
 
 default_app_config = "django_rest_passwordreset.apps.DjangoRestPasswordResetConfig"
